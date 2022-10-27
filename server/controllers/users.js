877fede52d45b7dfbf2bf1d8db73e6ca7351a125
KlearCj <klearmadera17@gmail.com>
2022-10-27 14:40:24 -0300

    cambios

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,41 +1,45 @@
-const Users = require('../db/models')
+const { generateToken } = require("../config");
+const Users = require("../db/models");
 
-const UsersCtrl={
-signup: async (req, res)=>{
-try{
-    await Users.create(req.body)
-    res.status(201).json({
+const UsersCtrl = {
+  signup: async (req, res) => {
+    try {
+      await Users.create(req.body);
+      res.status(201).json({
         message: `User has been created`,
-    })
-}
-catch{}
-},
-login: async (req,res)=>{
-const { email, password } = req.body;
-try{
-  const user= await Users.findOne({
-    where: { email },
-  })
-  
-}catch{}
-   /*  Users.findOne({
-      where: { email },
-    }).then((user) => {
-      if (!user) return res.sendStatus(401);
-  
-      user.validatePassword(password).then((isValid) => {
-        if (!isValid) return res.sendStatus(401);
-        else {
-          const payload = {
-            email: user.email,
-            username: user.username,
-          };
-          const token = generateToken(payload);
-          res.cookie("token", token).send(payload);
-        }
       });
-    }); */
-}
-}
+    } catch {
+      res.sendStatus(400);
+    }
+  },
+  login: async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      const user = await Users.findOne({
+        where: { email },
+      });
+      await user.validatePass(password);
+      const payload = user.email;
+      const token = generateToken(payload);
+      res.cookie("token", token).send(payload);
+    } catch {
+      return res.sendStatus(401);
+    }
+  },
+  logout: (req, res) => {
+    res.clearCookie("jwt");
+    res.sendStatus(204);
+  },
+  updateUser: async (req, res) => {
+    try {
+      const user = await Users.findOne(req.body);
+      await user.update(req.body);
+      await user.save();
+    } catch {
+        res.sendStatus(401)
+    }
+  },
+};
 
-module.exports= UsersCtrl
+module.exports = UsersCtrl;
+
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,9 +3,7 @@ const {validateToken} = require('../config')
 validateCookie=(req,res,next)=>{
     const token = req.cookies.token;
     const { payload } = validateToken(token);
-  
     req.user = payload;
-  
     if (payload) return next();
     res.sendStatus(401);
 }
